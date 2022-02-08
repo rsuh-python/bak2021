deea17f28d149c0ea0aee6478b037d7d85450a96
fortvivlan <jahil.kematian@gmail.com>
2022-02-08 16:01:44 +0300

    start new semester

---
+++ 01/main.py
@@ -0,0 +1,10 @@
+import module 
+# from module import x
+
+
+def func(y): 
+    # x = 7
+    return y
+
+x = 6
+print(func(x), module.x, x)
---
+++ 01/module.py
@@ -0,0 +1,7 @@
+x = 5
+
+def sqrt(x):
+    return x ** 0.5
+
+if __name__ == '__main__':
+    print('Hello world!') 
